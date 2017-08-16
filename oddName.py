867041617fbc46d3b74a75e5be851ba1b336f0eb
David plummer <david.plummer1@my.jcu.edu.au>
2017-08-16 16:52:29 +1000

    """Added Name checking and input"""

--- oddName.py
+++ oddName.py
@@ -1,1 +1,10 @@
 """David Plummer"""
+
+def my_name():
+    name = input("Enter your Name: ")
+    while name == "":
+        print("Invalid input")
+        name = input("Enter your Name: ")
+
+    print(name[::2])
+my_name()
